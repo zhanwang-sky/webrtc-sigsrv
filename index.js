eedcbb0d4c0adb789fb1d304feeae678933255aa
zhanwang-sky <zhanwang-sky@users.noreply.github.com>
2023-07-18 21:15:39 +0800

    basic peerConnection

--- index.js
+++ index.js
@@ -1,46 +1,87 @@
 const express = require('express');
 const http = require('http');
 const log4js = require('log4js');
-const socketIO = require('socket.io');
+const socketIo = require('socket.io');
 
 log4js.configure({
-    appenders: { console: { type: 'console'} },
-    categories: { default: { appenders: ["console"], level: "TRACE" } }
+  appenders: { console: { type: 'console'} },
+  categories: { default: { appenders: ["console"], level: "TRACE" } }
 });
 const logger = log4js.getLogger('sigsrv');
 
 if (process.argv.length != 3) {
-    logger.error(`Usage: node ./index.js <port>`);
-    process.exit(1);
+  logger.error(`Usage: node ./index.js <port>`);
+  process.exit(1);
 }
 const port = parseInt(process.argv[2], 10);
 
 const app = express();
 const http_srv = http.createServer(app);
-const io = socketIO(http_srv);
+const io = socketIo(http_srv);
 
 io.on('connection', (socket) => {
-    logger.debug('user connected');
+  logger.debug(`user ${socket.id} connected`);
 
-    socket.on('join', (room) => {
-        logger.debug(`user join room ${room}`);
-    });
+  socket.on('join', (roomId, ack) => {
+    try {
+      logger.debug(`user ${socket.id} joins room ${roomId}`);
+      let myRoom = io.sockets.adapter.rooms.get(roomId);
+      let usrCnt = myRoom ? myRoom.size : 0;
+      logger.debug(`room ${roomId}: usrCnt=${usrCnt}`);
+      if (usrCnt >= 2) {
+        ack('full');
+        throw new Error('room is full');
+      }
+      socket.join(roomId);
+      ack(usrCnt + 1);
+      socket.to(roomId).emit('join_notify', socket.id);
+    } catch (err) {
+      logger.error(`Fail to process join request: ${err}`);
+    }
+  });
 
-    socket.on('leave', (room) => {
-        logger.debug(`user leave room ${room}`);
-    });
+  socket.on('leave', (roomId, ack) => {
+    try {
+      logger.debug(`user ${socket.id} leaves room ${roomId}`);
+      let myRoom = io.sockets.adapter.rooms.get(roomId);
+      let usrCnt = myRoom ? myRoom.size : 0;
+      logger.debug(`room ${roomId}: usrCnt=${usrCnt}`);
+      if (usrCnt <= 0) {
+        ack('empty');
+        throw new Error('room is empty');
+      }
+      socket.leave(roomId);
+      ack(usrCnt - 1);
+      socket.to(roomId).emit('leave_notify', socket.id);
+    } catch (err) {
+      logger.error(`Fail to process leave request: ${err}`);
+    }
+  });
 
-    socket.on('sdp', (room, data) => {
-        logger.debug(`user sdp: room=${room}, data=${data}`);
-    });
+  socket.on('message', (roomId, msg) => {
+    try {
+      logger.debug(`user ${socket.id} sent message to room ${roomId}: ${msg}`);
+      socket.to(roomId).emit('message', msg);
+    } catch (err) {
+      logger.error(`Fail to process message request: ${err}`);
+    }
+  });
 
-    socket.on('disconnect', () => {
-        logger.debug('user disconnected');
-    });
+  // hasn't left its rooms yet.
+  socket.on('disconnecting', () => {
+    try {
+      logger.debug(`user ${socket.id} disconnecting...`);
+      let roomList = Array.from(socket.rooms);
+      logger.debug('sending leave_notify to rooms:', roomList);
+      socket.to(roomList).emit('leave_notify', socket.id);
+    } catch (err) {
+      logger.error(`Fail to process disconnecting event: ${err}`);
+    }
+  });
 });
 
 http_srv.listen(port, () => {
-    logger.info(`listening on *:${port}`);
+  logger.info(`listening on *:${port}`);
 });
 
 logger.info('All Done!');
